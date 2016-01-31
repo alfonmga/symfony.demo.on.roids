8896eac896b346152b6aa25dd9d06450102b294f
Alfonso M. García Astorga <hello@alfonsomga.com>
2016-01-31 22:57:08 +0100

    Enable umask as temporal fix for Vagrant

--- web/app.php
+++ web/app.php
@@ -8,6 +8,11 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
+// If you don't want to setup permissions the proper way, just uncomment the
+// following PHP line. See:
+// http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
+umask(0000);
+
 /**
  * @var Composer\Autoload\ClassLoader
  */
--- web/app_dev.php
+++ web/app_dev.php
@@ -12,7 +12,7 @@ use Symfony\Component\Debug\Debug;
 // If you don't want to setup permissions the proper way, just uncomment the
 // following PHP line. See:
 // http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
-//umask(0000);
+umask(0000);
 
 // This check prevents access to debug front controllers that are deployed by
 // accident to production servers. Feel free to remove this, extend it, or make
