7f4c9faf246a51462d609298f24b5033a231d53d
Alfonso M <me@alfon.io>
2016-02-17 13:27:59 +0100

    Revert "Populate ElasticSearch indices manually isn't needed"
    Related issue #12

--- src/ElasticSearchBundle/Tests/Controller/ElasticSearchControllerTest.php
+++ src/ElasticSearchBundle/Tests/Controller/ElasticSearchControllerTest.php
@@ -39,6 +39,8 @@ class ElasticSearchControllerTest extends WebTestCase
         $this->entityManager->persist($post);
         $this->entityManager->flush();
 
+        self::populateElasticSearchIndices();
+
         $crawler = $client->request('GET', '/blog/search-results?q=Elasticsearch rocks ' . $randnumber);
         $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
         $this->assertEquals(
